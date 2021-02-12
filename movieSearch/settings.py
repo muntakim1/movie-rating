eaa41d468ff701629e3cc6a99ae97276ef0fc611
muntakim1 <muntakim1104001@gmail.com>
2021-02-13 02:56:32 +0600

    saved

--- movieSearch/settings.py
+++ movieSearch/settings.py
@@ -157,7 +157,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # CORS_ALLOWED RULES
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
